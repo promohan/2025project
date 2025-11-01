eb2fd51a44cb309e7de927d694847ebe0ba84480
promohan <mohannhc2005@gmail.com>
2025-11-01 14:20:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+// document.write("hello");
+    //    let a=551;
+    function mohan2() {
+        document.getElementById('open-div').style.display= "none";
+        document.getElementById('close').style.display="none";
+    }
+    function mohan3(){
+        document.getElementById('open-div').style.display="flex";
+        document.getElementById('close').style.display="block";
+
+    }
+    // function mohan3(){
+    //     document.getElementById('open-div').style.display="";
+    // }
+
+    // const closeMenu=document.querySelector('#hambergarh');
+    // const closeMenu=document.getElementById('hambergarh')
+    // closeMenu.addEventListener('click',(e)=>{
+    //     closeMenu.style="display:none"
+    // })
+    
